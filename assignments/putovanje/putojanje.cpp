97ab3f703ca23e96c5a5da06b73c4f33acd3e4c4
SKajiwara <gold.shin789@gmail.com>
2019-09-20 13:11:13 -0600

    final ver of putojanje

--- assignments/putovanje/putojanje.cpp
+++ assignments/putovanje/putojanje.cpp
@@ -5,33 +5,25 @@
 #include <algorithm>
 #include <iterator>
 using namespace std;
-void test()
+int answer(vector<int>& list, const int Fmax)
 {
-	cout << "a";
-}
-void kattis()
-{
-	int i, Fnum, Fmax, in, ans, temp, count = 1;
-	vector<int> list;
-	vector<int>::iterator it, head;
-	cin >> Fnum >> Fmax;
-	for ( i=0; i<Fnum; i++ )
-	{	
-		cin >> in;
-		list.push_back( in );
-	}
-	it = list.begin();
-	while( distance(it,list.end())>1 && distance(it,list.end())>=count )
+	int ans, temp = 0;
+	int count = 0;
+	vector<int>::iterator head, it = list.begin();
+	// When capacity C is 1 and there is fruit weights 1 in Forest 
+	if (Fmax==1 && find(list.begin(), list.end(), 1) != list.end()) return 1;
+	while( (distance(it,list.end())>1 && distance(it,list.end())>=count) )
 	{
 		ans = *it;
 		head = it+1;
-		temp = 1;
-		while (ans < Fmax && distance(head,list.end())>= 1)
+		if( Fmax > ans) temp = 1;
+		while (distance(head,list.end())>= 1)
 		{
 			if( Fmax >= (ans + *head)) 
 			{	
 				ans += *head;
 				temp++;
+		
 				head++;
 			}
 			else head++;
@@ -39,7 +31,25 @@ void kattis()
 		if (temp>count) count = temp;
 		it++;
 	}
-	cout << count << endl;
+	return count;
+}
+void test()
+{
+	vector<int> test = {4, 5, 3, 1, 6, 3, 2, 4, 1, 4, 5, 4, 2, 1, 4};
+	cout << answer(test, 1);
+	test = {4, 5, 3, 1, 6, 3, 2, 4, 1, 4, 5, 4, 2, 1, 4};
+	assert(answer(test, 10) == 5);
+	test = {8};
+	assert(answer(test, 5 ) == 0);
+	
+}
+void kattis()
+{
+	int i, Fnum, Fmax, in;
+	vector<int> list;
+	cin >> Fnum >> Fmax;
+	for ( i=0; i<Fnum&&cin >> in; i++ ) list.push_back( in );
+	cout << answer(list, Fmax)<< endl;
 }
 int main(int argc, char* argv[])
 {
